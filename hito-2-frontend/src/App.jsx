9d8cfddce7ddd0782fc4ad3321f9267e5cec1d55
eduardo jara <eduardojaramardones@gmail.com>
2026-03-21 16:56:27 -0300

    fix footer import case sensitive

--- hito-2-frontend/src/App.jsx
+++ hito-2-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/NavBar"
-import Footer from "./components/footer"
+import Footer from "./components/Footer"
 
 import Home from "./pages/Home"
 import Gallery from "./pages/Gallery"
